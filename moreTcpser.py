39a16132ca4775ebccb8a124bca0cd2623991c66
wolflop <35335371+wolflop@users.noreply.github.com>
2019-06-23 17:05:56 +0800

    Update moreTcpser.py

--- moreTcpser.py
+++ moreTcpser.py
@@ -24,19 +24,19 @@ def Tcp_Server(PORT):
             tcpCliSock.send(data)               #将收到的数据发送给客户端
         tcpCliSock.close()                      #关闭连接
     tcpSerSock.close()
-
+#创建线程检查函数
 def checkThread(sleeptimes, initThreadName=[]):
-    for i in range(400, 10000, 1):
+    for i in range(400, 10000, 1):  #线程在400-10000循环
         nowThreadName=[]
         now = threading.enumerate() #获取当前线程名
         for i in now:
             nowThreadName.append(i.getName()) #保存当前线程名字
         for i in initThreadsName:
             if i in nowThreadName:
-                pass #当前某线程在初始化线程组里，不座操作
+                pass #当前某线程在初始化线程组里，不做操作
             else:
                 print("%s stop, now restart" %i)
-                t = threading.Thread(target=Tcp_Server, args=(i, )) #当当前线程不在初始化线程组里，重启线程
+                t = threading.Thread(target=Tcp_Server, args=(i, )) #如果当前线程不在初始化线程组里，重启线程
                 t.setName(i) #重设线程名称
                 t.start()
         time.sleep(sleeptimes) #隔一段时间重新运行，检测有没有线程down
