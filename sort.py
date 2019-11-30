e23257997585bc3e051f90d7b3a455d3df0a45c3
wolflop <35335371+wolflop@users.noreply.github.com>
2019-11-30 15:16:11 +0800

    Update sort.py

--- sort.py
+++ sort.py
@@ -12,7 +12,7 @@ class solution:
     插入排序：假定n-1个元素已经排序完成，第n个元素插入到正确的位置上
     '''
     def ins_sort(self):
-        for i in range(len(self.seq)):                  #i在self.seq中循环
+        for i in range(len(self.seq)):                  #选择i在self.seq中从0到len（self.seq）递增
             j = i                                       #选出j，j=i，让j递减，在i的范围内比较大小，按照从小到大排序
             while j > 0 and seq[j-1] > seq[j]: 
                 seq[j-1], seq[j] = seq[j], seq[j-1]
@@ -20,10 +20,13 @@ class solution:
          return self.seq                                #返回排序的self.seq
       
       #迭代的方式选择排序
+      '''
+      先找到其中最大的元素放到n的位置上，然后继续比较余下的值。
+      '''
       def sel_sort(self):
         for i in range(len(self.seq)-1, 0, -1):         #选择i在self.seq中循环，从到小，递减
             max_j = i                                   #先将i赋值给max_j
-            for j in range(i):                          #j在i中循环，选出最大的max_j,
+            for j in range(i):                          #j在i中循环，选出最大的max_j,依次从大到小排序
                 if self.seq[j] > self.seq[max_j]:
                     max_j = j
                 self.seq[i], self.seq[max_j] = self.seq[max_j], self.seq[i]
