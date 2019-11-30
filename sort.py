6a641309b2beca788b2de8a816a34e2b573cf33e
wolflop <35335371+wolflop@users.noreply.github.com>
2019-11-30 15:06:36 +0800

    Create sort.py

---
+++ sort.py
@@ -0,0 +1,37 @@
+# -*- coding:utf-8 -*-
+#filename:sort.py
+'''
+本段代码写入很多排序实例
+'''
+class solution:
+    def __init__(self, seq):
+        self.seq = seq
+     
+    #迭代的方式插入排序
+    '''
+    插入排序：假定n-1个元素已经排序完成，第n个元素插入到正确的位置上
+    '''
+    def ins_sort(self):
+        for i in range(len(self.seq)):                  #i在self.seq中循环
+            j = i                                       #选出j，j=i，让j递减，在i的范围内比较大小，按照从小到大排序
+            while j > 0 and seq[j-1] > seq[j]: 
+                seq[j-1], seq[j] = seq[j], seq[j-1]
+                j -= 1
+         return self.seq                                #返回排序的self.seq
+      
+      #迭代的方式选择排序
+      def sel_sort(self):
+        for i in range(len(self.seq)-1, 0, -1):         #选择i在self.seq中循环，从到小，递减
+            max_j = i                                   #先将i赋值给max_j
+            for j in range(i):                          #j在i中循环，选出最大的max_j,
+                if self.seq[j] > self.seq[max_j]:
+                    max_j = j
+                self.seq[i], self.seq[max_j] = self.seq[max_j], self.seq[i]
+        return self.seq
+        
+        
+        
+      
+    
+  
+
